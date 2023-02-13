3d38569b2a7231af72bebc8c0a953ff92200ece8
hhertout <hugues.hertout@gmail.com>
2023-02-13 21:08:16 +0100

    init badger

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -21,3 +21,22 @@ func (e Entry) String() string {
 	created := e.CreatedAt.Format(time.Stamp)
 	return fmt.Sprintf("%-10v\t%-50v%-6v", e.Word, e.Definition, created)
 }
+
+func New(dir string) (*Dictionary, error) {
+	opts := badger.DefaultOptions(dir)
+
+	db, err := badger.Open(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	dict := &Dictionary{
+		db: db,
+	}
+
+	return dict, nil
+}
+
+func (d *Dictionary) Close() {
+	d.db.Close()
+}
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"training/dictionary_project/dictionary"
+)
 
+func main() {
+	d, err := dictionary.New("./badger")
+	handleErr(err)
+
+	defer d.Close()
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Dictionary error: %v\n", err)
+	}
 }
